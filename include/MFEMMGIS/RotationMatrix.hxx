ca1a7abb3f00781c3c1c3670102cfc6deff588df
Thomas Helfer <thomas.helfer@cea.fr>
2021-01-15 22:27:12 +0100

    fix RotationMatrix3D

--- include/MFEMMGIS/RotationMatrix.hxx
+++ include/MFEMMGIS/RotationMatrix.hxx
@@ -26,20 +26,19 @@ namespace mfem_mgis {
       std::variant<std::monostate,
                    std::array<real, 9u>,
                    std::shared_ptr<PartialQuadratureFunction>>;
+  //! a simple alias
+  using MaterialAxis3D =
+      std::variant<std::array<real, 3u>,
+                   std::shared_ptr<PartialQuadratureFunction>>;
   /*!
    * \brief definition of the rotation matrix in 3D
    * The rotation matrix is defined by:
    * - a constant matrix, computed once for all
-   * - the two directions of orthotropy which can be given either as a partial
-   *   function or as a fixed direction.
+   * - the two directions of orthotropy which can be given either as a
+   * partial function or as a fixed direction.
    */
   using RotationMatrix3D =
-      std::variant<std::monostate,
-                   std::array<real, 9u>,
-                   std::variant<std::array<real, 3u>,
-                                std::shared_ptr<PartialQuadratureFunction>>,
-                   std::variant<std::array<real, 3u>,
-                                std::shared_ptr<PartialQuadratureFunction>>>;
+      std::variant<std::monostate, std::array<MaterialAxis3D, 2u>>;
 
 };  // end of namespace mfem_mgis
 
