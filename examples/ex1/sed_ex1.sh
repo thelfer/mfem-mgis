ae60ed815c9afddc0da6857cbabbb9e68e2ed03a
LATU Guillaume 224549 <guillaume.latu@cea.fr>
2021-03-24 17:22:18 +0100

    double quote for parameter

--- examples/ex1/sed_ex1.sh
+++ examples/ex1/sed_ex1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sed "s%mesh_file = nullptr%mesh_file = \"cube.mesh\"%g;s%behaviour = nullptr%behaviour = \"Plasticity\"%g;s%library = nullptr%library = \"src/libBehaviour.so\"%g;s%reference_file = nullptr%reference_file = \"Plasticity.ref\"%g;s%isv_name = nullptr%isv_name = \"EquivalentPlasticStrain\"%g" $1 > $2
+sed "s%mesh_file = nullptr%mesh_file = \"cube.mesh\"%g;s%behaviour = nullptr%behaviour = \"Plasticity\"%g;s%library = nullptr%library = \"src/libBehaviour.so\"%g;s%reference_file = nullptr%reference_file = \"Plasticity.ref\"%g;s%isv_name = nullptr%isv_name = \"EquivalentPlasticStrain\"%g" "$1" > "$2"
 
