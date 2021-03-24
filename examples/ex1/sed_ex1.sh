e75cff0adc36a0ce24cc1d3678d04a3024b6a409
LATU Guillaume 224549 <guillaume.latu@cea.fr>
2021-03-24 17:26:55 +0100

    simplify sed_ex1.sh

--- examples/ex1/sed_ex1.sh
+++ examples/ex1/sed_ex1.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-sed "s%mesh_file = nullptr%mesh_file = \"cube.mesh\"%g;s%behaviour = nullptr%behaviour = \"Plasticity\"%g;s%library = nullptr%library = \"src/libBehaviour.so\"%g;s%reference_file = nullptr%reference_file = \"Plasticity.ref\"%g;s%isv_name = nullptr%isv_name = \"EquivalentPlasticStrain\"%g" "$1" > "$2"
-
+sed \
+    -e "s%mesh_file = nullptr%mesh_file = \"cube.mesh\"%g" \
+    -e "s%behaviour = nullptr%behaviour = \"Plasticity\"%g" \
+    -e "s%library = nullptr%library = \"src/libBehaviour.so\"%g" \
+    -e "s%reference_file = nullptr%reference_file = \"Plasticity.ref\"%g" \
+    -e "s%isv_name = nullptr%isv_name = \"EquivalentPlasticStrain\"%g" \
+    "$1" > "$2"
+    
