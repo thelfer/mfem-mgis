c876a5076a2ead377ed3f74582c7a4236cf9680a
Thomas Helfer <thomas.helfer@cea.fr>
2021-03-28 21:58:36 +0200

    Parallel version of the ComputeResultantForceOnBoundary post-processing

--- src/ComputeResultantForceOnBoundary.cxx
+++ src/ComputeResultantForceOnBoundary.cxx
@@ -42,6 +42,47 @@ namespace mfem_mgis {
 
 #ifdef MFEM_USE_MPI
 
+  ComputeResultantForceOnBoundary<true>::ComputeResultantForceOnBoundary(
+      NonLinearEvolutionProblemImplementation<true>& p,
+      const Parameters& params)
+      : ComputeResultantForceOnBoundaryCommon(
+            buildFacesDescription<true>(p, get<int>(params, "Boundary")),
+            get<int>(params, "Boundary")) {
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+      const auto& f = get<std::string>(params, "OutputFileName");
+      this->out.open(f);
+      if (!this->out) {
+        mgis::raise("can't open file '" + f + "'");
+      }
+      auto& fed = p.getFiniteElementDiscretization();
+      auto& fes = fed.template getFiniteElementSpace<true>();
+      writeOuputFileHeader(this->out, this->bid, fes.GetVDim());
+    }
+  }  // end of ComputeResultantForceOnBoundary
+
+  void ComputeResultantForceOnBoundary<true>::execute(
+      NonLinearEvolutionProblemImplementation<true>& p,
+      const real t,
+      const real dt) {
+    mfem::Vector F;
+    computeResultantForceOnBoundary(F, p, this->faces);
+    //
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    if (rank == 0) {
+      mfem::Vector gF;
+      gf.SetSize(F.GetSize());
+      MPI_Reduce(F.data(), gF.data(), F.GetSize(), MPI_DOUBLE, MPI_SUM, 0,
+                 MPI_COMM_WORLD);
+      writeResultantForce(this->out, gF, t + dt);
+    }
+  }  // end of execute
+
+  ComputeResultantForceOnBoundary<true>::~ComputeResultantForceOnBoundary() =
+      default;
+
 #endif /* MFEM_USE_MPI */
 
   ComputeResultantForceOnBoundary<false>::ComputeResultantForceOnBoundary(
