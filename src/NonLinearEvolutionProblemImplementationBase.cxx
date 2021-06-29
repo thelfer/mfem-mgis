8bf89605cf54409ee7ef057a37a9f4b7bef73247
LATU Guillaume 224549 <guillaume.latu@cea.fr>
2021-06-29 13:06:51 +0200

    [BUGFIX] Restore the availability of preconditioner whenever using Hypre linear solver

--- src/NonLinearEvolutionProblemImplementationBase.cxx
+++ src/NonLinearEvolutionProblemImplementationBase.cxx
@@ -231,12 +231,14 @@ namespace mfem_mgis {
       std::unique_ptr<LinearSolverPreconditioner> p) {
     if (p != nullptr) {
       auto* const isolver = dynamic_cast<IterativeSolver*>(s.get());
-      if (isolver == nullptr) {
-        mgis::raise(
-            "NonLinearEvolutionProblemImplementationBase::updateLinearSolver: "
-            "can't associate a preconditioner to a non iterative solver");
+      if (isolver != nullptr) {
+	isolver->SetPreconditioner(*p);
       }
-      isolver->SetPreconditioner(*p);
+//      if (isolver == nullptr) {
+//        mgis::raise(
+//            "NonLinearEvolutionProblemImplementationBase::updateLinearSolver: "
+//            "can't associate a preconditioner to a non iterative solver");
+//      }
       this->updateLinearSolver(std::move(s));
       this->linear_solver_preconditioner = std::move(p);
     } else {
