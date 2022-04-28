530fd8e74060f10ecaab0d19e9579e9d65122cb7
LATU Guillaume 224549 <guillaume.latu@cea.fr>
2022-04-28 09:03:04 +0200

    [BUGFIX] Restore flags for handling Hypre versions prior to 2021

--- src/LinearSolverFactory.cxx
+++ src/LinearSolverFactory.cxx
@@ -63,6 +63,7 @@ namespace mfem_mgis {
 
   std::unique_ptr<LinearSolverPreconditioner> setHypreILUPreconditioner(
       NonLinearEvolutionProblemImplementation<true>&, const Parameters& opts) {
+#ifndef HYPRE_OLD_VERSION
     using Problem = AbstractNonLinearEvolutionProblem;
     bool verbose = contains(opts, Problem::SolverVerbosityLevel);
     auto ilu = std::make_unique<mfem::HypreILU>();
@@ -77,6 +78,10 @@ namespace mfem_mgis {
       HYPRE_ILUSetLevelOfFill(*ilu,level);
     }
     return ilu;
+#else /*  HYPRE_OLD_VERSION */
+    MFEM_VERIFY(0, "Support for HypreILU is notavailable with this version of MFEM");
+    return nullptr;
+#endif /* HYPRE_OLD_VERSION */
   }    // end of setHypreILUPreconditioner
 
   std::unique_ptr<LinearSolverPreconditioner> setHypreParaSailsPreconditioner(
