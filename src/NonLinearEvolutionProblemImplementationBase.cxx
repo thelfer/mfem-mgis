44466ef3b1ca74e39966f58c89c960b81aa94da0
Thomas Helfer <thomas.helfer@cea.fr>
2026-03-03 07:40:19 +0100

    clang-format

--- src/NonLinearEvolutionProblemImplementationBase.cxx
+++ src/NonLinearEvolutionProblemImplementationBase.cxx
@@ -334,19 +334,19 @@ namespace mfem_mgis {
       Context& ctx, const Parameters& params) noexcept {
     try {
 #ifdef MFEM_USE_PETSC
-    if (usePETSc()) {
-      mfem_mgis::setSolverParameters(*(this->petsc_solver), params);
-    } else {
-      mfem_mgis::setSolverParameters(*(this->solver), params);
-    }
+      if (usePETSc()) {
+        mfem_mgis::setSolverParameters(*(this->petsc_solver), params);
+      } else {
+        mfem_mgis::setSolverParameters(*(this->solver), params);
+      }
 #else  /* MFEM_USE_PETSC */
-    mfem_mgis::setSolverParameters(*(this->solver), params);
+      mfem_mgis::setSolverParameters(*(this->solver), params);
 #endif /* MFEM_USE_PETSC */
     } catch (...) {
       return mgis::registerExceptionInErrorBacktrace(ctx);
     }
     return true;
-  }    // end of setSolverParameters
+  }  // end of setSolverParameters
 
   void NonLinearEvolutionProblemImplementationBase::setSolverParameters(
       const Parameters& params) {
